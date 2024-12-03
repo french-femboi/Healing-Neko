a33f2b7142386b54cc094aad687546a25c6baf93
catpawz <cat@catpawz.net>
2024-12-03 20:09:29 +0100

    updated dart dependencies and updated readme

--- lib/main/home.dart
+++ lib/main/home.dart
@@ -1654,7 +1654,7 @@ class _homePagePageState extends State<homePagePage> {
                             vibrate();
                             playUiSound(2);
                             saveIndex(4);
-                            launchUrl(Uri.parse('https://apps.french-femboi.eu/view?app=healing-neko'));
+                            launchUrl(Uri.parse('https://apps.catpawz.net/view?app=healing-neko'));
                           },
                           style: ElevatedButton.styleFrom(
                             backgroundColor: const Color(0xFF332841),
@@ -1730,7 +1730,7 @@ class _homePagePageState extends State<homePagePage> {
                             vibrate();
                             playUiSound(2);
                             saveIndex(4);
-                            launchUrl(Uri.parse('https://github.com/french-femboi/Healing-Neko'));
+                            launchUrl(Uri.parse('https://github.com/catpawzz/Healing-Neko'));
                           },
                           style: ElevatedButton.styleFrom(
                             backgroundColor: const Color(0xFF332841),
